8027bde372a0b7a00c55dda24349e343788a0166
Johan Robles <johanrobles66@yahoo.com>
2022-04-29 11:04:50 -0500

    Actalizacion, agregando paredes y obstaculos

--- Snake_Game.py
+++ Snake_Game.py
@@ -13,8 +13,9 @@ piton = turtle.Turtle()
 piton.color('#009982')
 piton.penup()
 piton.shape('circle')
+piton.speed(0)
+piton.goto(-150, -140)
 piton.speed(2)
-piton.goto(0, 0)
 # Creamos una variable que nos indicara la direccion del movimento de la serpiente, en un principio sere stop, no se movera.
 posicion = 'stop'
 
@@ -49,7 +50,54 @@ def left():
     posicion = 'left'
 def right():
     global posicion
-    posicion = 'right'     
+    posicion = 'right'
+
+# creamos otro ´puntor para que nos cree las paredes dentro de la pantalla.
+pared = turtle.Turtle()
+pared.shape('square')
+pared.color('white')
+pared.penup()
+# pared.ht()
+pared.speed(0)
+pared.goto(-215, 180)
+pared.pensize(20)
+pared.pendown()
+for i in range(1, 5):
+    if i % 2 != 0:
+        pared.forward(425)
+        pared.right(90)
+        continue
+    pared.forward(390)
+    pared.right(90)
+pared.right(90)
+pared.forward(240)
+pared.right(270)
+pared.forward(50)
+pared.penup()
+pared.forward(70)
+pared.pendown()
+pared.forward(30)
+pared.right(270)
+pared.forward(80)
+pared.penup()
+pared.forward(70)
+pared.pendown()
+pared.forward(90)
+pared.right(90)
+pared.forward(150)
+pared.right(90)
+pared.forward(80)
+pared.right(180)
+pared.forward(80)
+pared.right(90)
+pared.forward(125)
+pared.right(90)
+pared.forward(250)
+pared.right(90)
+pared.forward(140)
+pared.right(270)
+pared.forward(40)
+pared.ht()
 
 # Ahora creams nustras funciones de turtle que hacer que cuando se presionen las teclas (arriba, abajo, derecha y izquierda), el valor de posicion
 # cambia segun estas, de tal manera que nuestro puntor piton se movera hacia la direccion que queramos.
@@ -64,8 +112,7 @@ while True:
     # dentro de este ciclo estara llamandose la funcion movimiento que hara que se mueva el piton de acuedor a las teclas que presionemos.
     # y es muy importante ir actualizando la pantalla constantemnete ya que asi se actualizaria el nuevo movimiento de la piton y podriamos 
     # visalizar como se mueve.
-    movimientos()
+
     juego.update()
-
-
-turtle.done()
+    movimientos()
+    juego.delay(30)
